9362472d92461660e27e5136a12c91c5b7c75d87
rene_mbp <harryloveslearning@googlemail.com>
2020-11-01 19:40:06 +0100

    first excalidraw-plugin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+const Preview = ({ snapshot }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (!ref.current) return;
+    const ctx = ref.current.getContext("2d");
+    if (!ctx) return;
+    ctx.putImageData(snapshot.imageData, 0, 0);
+  }, [snapshot]);
+  return <canvas ref={ref} width={snapshot.width} height={snapshot.height} />;
+};
+
+const App = () => {
+  const [snapshot, setSnapshot] = useState(null);
+
+  useEffect(() => {
+    console.log("init plugin");
+
+    // got message from excalidraw-core
+    const messageHandler = (message) => {
+      if (message?.data?.source === "excalidraw") {
+        switch (message.data.type) {
+          case "createSnapshot":
+            setSnapshot(message.data);
+            break;
+          default:
+        }
+      }
+    };
+    window.addEventListener("message", messageHandler);
+    return () => {
+      window.removeEventListener("message", messageHandler);
+    };
+  }, []);
+
+  // send message to excalidraw-core
+  const handleClick = () => {
+    if (window.parent) {
+      window.parent.postMessage(
+        { source: "excalidraw-plugin", type: "createSnapshot" },
+        "*"
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>first excalidraw-plugin</p>
+      <button onClick={handleClick}>take snapshot</button>
+
+      {snapshot && <Preview snapshot={snapshot} />}
     </div>
   );
-}
+};
 
 export default App;
